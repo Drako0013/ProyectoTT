2668b4f0e4156163a320a4503bae9d2a34b51f0c
Ethan Jimenez <erosethanjv@gmail.com>
2015-01-11 19:47:42 -0600

    Frame class fixed

--- AvanceEthan/Trabajo_Terminal_I/frame.cpp
+++ AvanceEthan/Trabajo_Terminal_I/frame.cpp
@@ -1,71 +1,61 @@
 #include "frame.h"
 
-Frame::Frame(bool grayscale) : matrix_(), grayscale_(grayscale) {}
+Frame::Frame(bool grayscale)
+  : matrix_(), grayscale_(grayscale) {}
 
-Frame::Frame(cv::Mat* matrix, bool grayscale) : grayscale_(grayscale) {
+Frame::Frame(cv::Mat* matrix, bool grayscale)
+  : grayscale_(grayscale) {
   SetMatrix(matrix);
 }
 
-Frame::~Frame() {
-  matrix_.~Mat();
-}
+Frame::~Frame() {}
 
-int Frame::Rows() const{
+int Frame::Rows() const {
   return matrix_.rows;
 }
 
-int Frame::Columns() const{
+int Frame::Columns() const {
   return matrix_.cols;
 }
 
-int Frame::GetPixel(int x, int y) const{
-  //if (grayscale_) 
-	  return matrix_.at<uchar>(x, y);
-  //cv::Vec3b v = matrix_.at<cv::Vec3b>(x, y);
-  //return (v[2] << 16) + (v[1] << 8) + v[0];
+void Frame::Rescale(int width, int height) {
+  cv::Mat matrix_copy;
+  matrix_.copyTo(matrix_copy);
+  cv::resize(matrix_copy, matrix_,
+             cv::Size(width, height));
 }
 
-void Frame::SetPixel(int x, int y, int rgb) {
-  if (!grayscale_) {
-    matrix_.at<cv::Vec3b>(x, y)[0] = rgb & 255;
-    matrix_.at<cv::Vec3b>(x, y)[1] = (rgb >> 8) & 255;
-    matrix_.at<cv::Vec3b>(x, y)[2] = (rgb >> 16) & 255;
+int Frame::GetPixel(int x, int y) const {
+  if (grayscale_) return matrix_.at<uchar>(x, y);
+
+  // Casting RGB vector to simple integer
+  cv::Vec3b v = matrix_.at<cv::Vec3b>(x, y);
+  return (v[2] << 16) + (v[1] << 8) + v[0];
+}
+
+void Frame::SetPixel(int x, int y, int pixel) {
+  if (grayscale_) {
+    // Grayscale for non-colored images
+    matrix_.at<uchar>(x, y) = pixel;
   } else {
-    matrix_.at<uchar>(x, y) = rgb;
+    // Color space for colored images is RGB
+    matrix_.at<cv::Vec3b>(x, y)[0] = pixel & 255;
+    matrix_.at<cv::Vec3b>(x, y)[1] = (pixel >> 8) & 255;
+    matrix_.at<cv::Vec3b>(x, y)[2] = (pixel >> 16) & 255;
   }
 }
 
-cv::Mat Frame::GetMatrix() const{
+cv::Mat Frame::GetMatrix() const {
   return matrix_;
 }
 
+// Assuming BGR as default color space of Mat
 void Frame::SetMatrix(cv::Mat* matrix) {
-  matrix_.~Mat();
   if (grayscale_) {
-    cv::cvtColor(*matrix, matrix_, CV_RGB2GRAY);
+    // Transformation to grayscale Mat
+    cv::cvtColor(*matrix, matrix_, CV_BGR2GRAY);
   } else {
-    matrix->copyTo(matrix_);
+    // Transformation to RGB standard Mat
+    cv::cvtColor(*matrix, matrix_, CV_BGR2RGB);
   }
-}
-
-cv::Mat Frame::reduceImageSize(int desWidth, int desHeight){
-	cv::Mat res = cv::Mat(desHeight, desWidth, CV_8U);
-	double size, sum;
-	int blockHeight = this->Rows() / desHeight;
-	int blockWidth = this->Columns() / desWidth;
-
-	for(int i = 0, ir = 0; i < this->Rows() && ir < desHeight; i = i + blockHeight, ir++){
-		for(int j = 0, jr = 0; j < this->Columns() && jr < desWidth; j = j + blockWidth, jr++){
-			sum = 0.0;
-			size = 0.0;
-			for(int ii = i; ii < i + blockHeight; ii++){
-				for(int jj = j; jj < j + blockWidth; jj++){
-					sum += (double)this->GetPixel(ii, jj);
-					size += 1.0;
-				}
-			}
-			res.at<uchar>(ir, jr) = (uchar)(sum / size);
-		}
-	}
-	return res;
 }
--- AvanceEthan/Trabajo_Terminal_I/frame.h
+++ AvanceEthan/Trabajo_Terminal_I/frame.h
@@ -7,19 +7,28 @@
 
 class Frame {
  public:
+  // Constructors
   Frame(bool = false);
   Frame(cv::Mat*, bool = false);
+
+  // Destructor
   ~Frame();
 
+  // Size methods
   int Rows() const;
   int Columns() const;
+  void Rescale(int, int);
+
+  // Pixel methods
   int GetPixel(int, int) const;
   void SetPixel(int, int, int);
+
+  // Matrix methods
   cv::Mat GetMatrix() const;
   void SetMatrix(cv::Mat*);
-  cv::Mat reduceImageSize(int desWidth, int desHeight);
 
  private:
+  // Member variables
   cv::Mat matrix_;
   bool grayscale_;
 };
