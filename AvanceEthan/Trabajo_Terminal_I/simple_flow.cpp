1e0dcca35120aeb212f12ca817b17a771c8d89d4
Adonais Romero González <adonais.neoadonis@gmail.com>
2015-01-14 14:24:07 -0600

    Integration of multi-stage SF & refactoring

--- AvanceEthan/Trabajo_Terminal_I/simple_flow.cpp
+++ AvanceEthan/Trabajo_Terminal_I/simple_flow.cpp
@@ -41,11 +41,6 @@ double SimpleFlow::GetWd(int x0, int y0, int x, int y){
 	return std::exp( -norm / (2 * rd) );
 }
 
-double SimpleFlow::GetWc(Frame &f1, int x0, int y0, int x, int y){
-	double norm = (double)( f1.GetPixel(x0, y0) - f1.GetPixel(x, y) );
-	return std::exp( -norm / (2 * rc) );
-}
-
 double SimpleFlow::GetWc(cv::Mat &f1, int x0, int y0, int x, int y){
 	Frame f(&f1, true);
 	double norm = (double)( f.GetPixel(x0, y0) - f.GetPixel(x, y) );
@@ -89,13 +84,14 @@ void SimpleFlow::CrossBilateralFilter(cv::Mat &orig, Frame &edge, cv::Mat& confi
 	for (int x = 0; x < rows; ++x) {
 		double* ptr_dest = dest.ptr<double>(x);
 		double* ptr_orig = orig.ptr<double>(x);
-		double* ptr_wr = orig.ptr<double>(x);
+		double* ptr_wr = confidence.ptr<double>(x);
 		for (int y = 0; y < cols; ++y, ++ptr_dest, ++ptr_orig, ++ptr_wr) {
 			double result = 0.0f;
+			double wr = *ptr_wr;
 			for (int i = -n; i <= n; ++i) {
 				for (int j = -n; j <= n; ++j) {
 					if (x + i >= 0 && x + i < rows && y + j >= 0 && y + j < cols) {
-						result += (*ptr_orig) * GetWd(x, y, x + i, y + j) * GetWc(edge, x, y, x + i, y + j) * (*ptr_wr);
+						result += (*ptr_orig) * GetWd(x, y, x + i, y + j) * GetWc(edge, x, y, x + i, y + j) * wr;
 					}
 				}
 			}
@@ -114,74 +110,39 @@ cv::Mat SimpleFlow::UpscaleFlow(cv::Mat& flow, int new_cols, int new_rows, Frame
 	return new_flow;
 }
 
-void SimpleFlow::CalcStageFlow(Frame& cur, Frame& next, cv::Mat& flow_x, cv::Mat& flow_y) {
-	// TODO: Refactor code
-}
-
-void SimpleFlow::CalcMultiStageFlow(cv::Mat& vel_x, cv::Mat& vel_y) {
-	cv::Mat flow_x, flow_y, flow_inv_x, flow_inv_y, confidence, confidence_inv;
-
-	//For now...
-	confidence = cv::Mat::ones(frames[0]->GetMatrix().size(), CV_64F);
-	confidence_inv = cv::Mat::ones(frames[0]->GetMatrix().size(), CV_64F);
-
-	std::vector<Frame> pyramid_cur, pyramid_next;
-
-	BuildPyramid(*frames[0], pyramid_cur);
-	BuildPyramid(*frames[1], pyramid_next);
-
-	// Refactor SimpleFlow for one stage. We'll be using it a lot...
-	CalcStageFlow(pyramid_cur.back(), pyramid_next.back(), flow_x, flow_y);
-	CalcStageFlow(pyramid_next.back(), pyramid_cur.back(), flow_inv_x, flow_inv_y);
-
-	for (int l = pyramid_cur.size() - 2; l >= 0; --l) {
-		Frame cur = pyramid_cur[l];
-		Frame next = pyramid_next[l];
-		Frame p_cur = pyramid_cur[l + 1];
-		Frame p_next = pyramid_next[l + 1];
-
-		const int curr_rows = cur.Rows();
-		const int curr_cols = cur.Columns();
-
-		flow_x = UpscaleFlow(flow_x, curr_rows, curr_cols, p_cur, confidence);
-		flow_inv_x = UpscaleFlow(flow_x, curr_rows, curr_cols, p_next, confidence_inv);
-		flow_y = UpscaleFlow(flow_y, curr_rows, curr_cols, p_cur, confidence);
-		flow_inv_y = UpscaleFlow(flow_y, curr_rows, curr_cols, p_next, confidence_inv);
-
-
-		//CalcConfidence(cur, curr_to, flow, confidence, max_flow);
-		CalcStageFlow(cur, next, flow_x, flow_y);
-
-		//calcConfidence(curr_to, cur, flow_inv, confidence_inv, max_flow);
-		CalcStageFlow(next, cur, flow_inv_x, flow_inv_y);
-
-	}
-
-	vel_x = flow_x;
-	vel_y = flow_y;
-}
-
-
-void SimpleFlow::CalculateFlow(cv::Mat& vel_x, cv::Mat& vel_y) {
-
-	// Following code needs to be refactored:
-
+void SimpleFlow::BilateralFilter(cv::Mat flow_x, cv::Mat flow_y, Frame cur, Frame next, cv::Mat confidence, std::vector< std::vector<bool> >& isOccludedPixel) {
 	const int n = SimpleFlow::NeighborhoodSize;
+	int rows = flow_x.rows;
+	int cols = flow_x.cols;
+	for (int y = 0; y < rows; y++){
+		double* ptr_x_f = flow_x.ptr<double>(y);
+		double* ptr_y_f = flow_y.ptr<double>(y);
+		double* ptr_wr = confidence.ptr<double>(y);
+		for (int x = 0; x < cols; x++, ++ptr_x_f, ++ptr_y_f, ++ptr_wr){
+			if (!(isOccludedPixel[y][x])){
+				double wr = *ptr_wr;
+				*ptr_x_f = 0.0;
+				*ptr_y_f = 0.0;
+				for (int u = -n; u <= n; ++u) {
+					for (int v = -n; v <= n; ++v) {
+						if (x + u < 0 || x + u >= rows || y + v < 0 || y + v >= cols) {
+							*ptr_x_f += GetWd(x, y, x + u, y + v) * GetWc(flow_x, x, y, x + u, y + v) * wr;
+							*ptr_y_f += GetWd(x, y, x + u, y + v) * GetWc(flow_y, x, y, x + u, y + v) * wr;
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+void SimpleFlow::CalcOcclusion(cv::Mat& vel_x, cv::Mat& vel_y, cv::Mat& vel_x_inv, cv::Mat& vel_y_inv, std::vector< std::vector<bool> >& isOccludedPixel) {
 
 	int rows = frames[0]->Rows();
 	int cols = frames[0]->Columns();
 
-	double e;
-	double E[n * 2 + 1][n * 2 + 1]; // Due to obvious limitations of arrays, E(u, v) is represented by E[u + n][v + n]
-	double Einv[n * 2 + 1][n * 2 + 1];
-	std::vector< std::vector<bool> > isOccludedPixel(rows, std::vector<bool>(cols));
+	isOccludedPixel = std::vector< std::vector<bool> >(rows, std::vector<bool>(cols));
 
-	vel_x = cv::Mat(rows, cols, CV_64F);
-	vel_y = cv::Mat(rows, cols, CV_64F);
-	cv::Mat vel_x_inv = cv::Mat(rows, cols, CV_64F);
-	cv::Mat vel_y_inv = cv::Mat(rows, cols, CV_64F);
-	cv::Mat velf_x = cv::Mat(rows, cols, CV_64F);
-	cv::Mat velf_y = cv::Mat(rows, cols, CV_64F);
 	for (int x = 0; x < rows; ++x) {
 		double* ptr_x = vel_x.ptr<double>(x);
 		double* ptr_y = vel_y.ptr<double>(x);
@@ -190,16 +151,43 @@ void SimpleFlow::CalculateFlow(cv::Mat& vel_x, cv::Mat& vel_y) {
 		double* ptr_y_inv = vel_y_inv.ptr<double>(x);
 
 		for (int y = 0; y < cols; ++y, ++ptr_x, ++ptr_y, ++ptr_x_inv, ++ptr_y_inv) {
+			double d_ptr_x = (*ptr_x - *ptr_x_inv);
+			double d_ptr_y = (*ptr_y - *ptr_y_inv);
+			double occlusion = sqrt((d_ptr_x * d_ptr_x) + (d_ptr_y * d_ptr_y));
+			//determine occludiness of pixels
+			isOccludedPixel[y][x] = (occlusion > occlusion_limit); //true if the pixel is occluded
+
+		}
+	}
+}
+
+void SimpleFlow::CalcStageFlow(Frame& cur, Frame& next, cv::Mat& vel_x, cv::Mat& vel_y) {
+	const int n = SimpleFlow::NeighborhoodSize;
+
+	int rows = frames[0]->Rows();
+	int cols = frames[0]->Columns();
+
+	double me;
+	double E[n * 2 + 1][n * 2 + 1]; // Due to obvious limitations of arrays, E(u, v) is represented by E[u + n][v + n]
+
+	vel_x = cv::Mat(rows, cols, CV_64F);
+	vel_y = cv::Mat(rows, cols, CV_64F);
+	for (int x = 0; x < rows; ++x) {
+		double* ptr_x = vel_x.ptr<double>(x);
+		double* ptr_y = vel_y.ptr<double>(x);
+
+		for (int y = 0; y < cols; ++y, ++ptr_x, ++ptr_y) {
 			for (int u = -n; u <= n; ++u) {
 				for (int v = -n; v <= n; ++v) {
 					if (x + u < 0 || x + u >= rows || y + v < 0 || y + v >= cols) {
 						E[u + n][v + n] = std::numeric_limits<double>::max();
-					} else {
-						E[u + n][v + n] = SimpleFlow::getSmoothness(*frames[0], *frames[1], x, y, u, v);
+					}
+					else {
+						E[u + n][v + n] = SimpleFlow::getSmoothness(cur, next, x, y, u, v);
 					}
 				}
 			}
-			double me = std::numeric_limits<double>::max();
+			me = std::numeric_limits<double>::max();
 			// TODO: Sub-pixel estimation (low priority)
 			for (int u = -n; u <= n; ++u) {
 				for (int v = -n; v <= n; ++v) {
@@ -210,71 +198,89 @@ void SimpleFlow::CalculateFlow(cv::Mat& vel_x, cv::Mat& vel_y) {
 					}
 				}
 			}
+		}
+	}
+}
 
-			// TODO: Bi-filter results; calculate occlusion (check)
-
-			//OcclusionCalculation
+void SimpleFlow::CalcConfidence(Frame& cur, Frame& next, cv::Mat& flow_x, cv::Mat& flow_y, cv::Mat& confidence) {
+	const int n = SimpleFlow::NeighborhoodSize;
+	std::vector<int> energyArray;
+	int rows = flow_x.rows;
+	int cols = flow_x.cols;
+	for (int y = 0; y < rows; y++){
+		double* ptr_x_f = flow_x.ptr<double>(y);
+		double* ptr_y_f = flow_y.ptr<double>(y);
+		double* ptr_wr = confidence.ptr<double>(y);
+		for (int x = 0; x < cols; x++, ++ptr_x_f, ++ptr_y_f, ++ptr_wr){
+			energyArray.clear();
 			for (int u = -n; u <= n; ++u) {
 				for (int v = -n; v <= n; ++v) {
 					if (x + u < 0 || x + u >= rows || y + v < 0 || y + v >= cols) {
-						Einv[u + n][v + n] = std::numeric_limits<double>::max();
-					} else {
-						Einv[u + n][v + n] = SimpleFlow::getSmoothness(*frames[1], *frames[0], x + u, y + v, u, v);
+						energyArray.push_back(GetEnergy(cur, x, y, next, x + u, y + v));
 					}
 				}
 			}
-			
-			//double me = std::numeric_limits<double>::max();
-			// TODO: Sub-pixel estimation (low priority)
-			for (int u = -n; u <= n; ++u) {
-				for (int v = -n; v <= n; ++v) {
-					if (Einv[u + n][v + n] < me) {
-						me = Einv[u + n][v + n];
-						*ptr_x_inv = u;
-						*ptr_y_inv = v;
-					}
-				}
-			}
-			double d_ptr_x = (*ptr_x - *ptr_x_inv);
-			double d_ptr_y = (*ptr_y - *ptr_y_inv);
-			double occlusion = sqrt( (d_ptr_x * d_ptr_x) + (d_ptr_y * d_ptr_y) );
-			//determine occludiness of pixels
-			isOccludedPixel[y][x] = (occlusion > occlusion_limit); //true if the pixel is occluded
-
+			*ptr_wr = GetWr(energyArray);
 		}
 	}
+}
 
-	//Bilateral filter
-	std::vector<int> energyArray;
+void SimpleFlow::CalculateFlow(cv::Mat& vel_x, cv::Mat& vel_y) {
 
-	for(int y = 0; y < rows; y++){
-		double* ptr_x_f = velf_x.ptr<double>(y);
-		double* ptr_y_f = velf_y.ptr<double>(y);
-		for(int x = 0; x < cols; x++, ++ptr_x_f, ++ptr_y_f){
-			if( !(isOccludedPixel[y][x]) ){
-				energyArray.clear();
-				for (int u = -n; u <= n; ++u) {
-					for (int v = -n; v <= n; ++v) {
-						if ( x + u < 0 || x + u >= rows || y + v < 0 || y + v >= cols ) {
-							if( !(isOccludedPixel[y + v][x + u]) ){
-								energyArray.push_back(GetEnergy(*frames[0], x, y, *frames[1], x + u, y + v));
-							}
-						}
-					}
-				}
-				double wr = GetWr(energyArray);
-				*ptr_x_f = 0.0;
-				*ptr_y_f = 0.0;
-				for (int u = -n; u <= n; ++u) {
-					for (int v = -n; v <= n; ++v) {
-						if ( x + u < 0 || x + u >= rows || y + v < 0 || y + v >= cols ) {
-							*ptr_x_f += GetWd(x, y, x + u, y + v) * GetWc(vel_x, x, y, x + u, y + v) * wr;
-							*ptr_y_f += GetWd(x, y, x + u, y + v) * GetWc(vel_y, x, y, x + u, y + v) * wr;
-						}
-					}
-				}
-			}
-		}
+	cv::Mat flow_x, flow_y, flow_inv_x, flow_inv_y, confidence, confidence_inv;
+
+	std::vector<Frame> pyramid_cur, pyramid_next;
+
+	BuildPyramid(*frames[0], pyramid_cur);
+	BuildPyramid(*frames[1], pyramid_next);
+
+	std::vector< std::vector<bool> > isOccluded(pyramid_cur.back().Rows(), std::vector<bool>(pyramid_cur.back().Columns()));
+	std::vector< std::vector<bool> > isOccludedInv(pyramid_cur.back().Rows(), std::vector<bool>(pyramid_cur.back().Columns()));
+
+	CalcStageFlow(pyramid_cur.back(), pyramid_next.back(), flow_x, flow_y);
+	CalcStageFlow(pyramid_next.back(), pyramid_cur.back(), flow_inv_x, flow_inv_y);
+
+	CalcOcclusion(flow_x, flow_y, flow_inv_x, flow_inv_y, isOccluded);
+	CalcOcclusion(flow_inv_x, flow_inv_y, flow_x, flow_y, isOccludedInv);
+
+	confidence = cv::Mat::ones(pyramid_cur.back().GetMatrix().size(), CV_64F);
+	confidence_inv = cv::Mat::ones(pyramid_cur.back().GetMatrix().size(), CV_64F);
+
+	BilateralFilter(flow_x, flow_y, *frames[0], *frames[1], confidence, isOccluded);
+	BilateralFilter(flow_inv_x, flow_inv_y, *frames[1], *frames[0], confidence_inv, isOccludedInv);
+
+	for (int l = pyramid_cur.size() - 2; l >= 0; --l) {
+		Frame cur = pyramid_cur[l];
+		Frame next = pyramid_next[l];
+		Frame p_cur = pyramid_cur[l + 1];
+		Frame p_next = pyramid_next[l + 1];
+
+		const int curr_rows = cur.Rows();
+		const int curr_cols = cur.Columns();
+
+		std::vector< std::vector<bool> > isOccluded(curr_rows, std::vector<bool>(curr_cols));
+		std::vector< std::vector<bool> > isOccludedInv(curr_rows, std::vector<bool>(curr_cols));
+
+		flow_x = UpscaleFlow(flow_x, curr_rows, curr_cols, p_cur, confidence);
+		flow_inv_x = UpscaleFlow(flow_x, curr_rows, curr_cols, p_next, confidence_inv);
+		flow_y = UpscaleFlow(flow_y, curr_rows, curr_cols, p_cur, confidence);
+		flow_inv_y = UpscaleFlow(flow_y, curr_rows, curr_cols, p_next, confidence_inv);
+
+		CalcConfidence(cur, next, flow_x, flow_y, confidence);
+		CalcConfidence(next, cur, flow_inv_x, flow_inv_y, confidence_inv);
+
+		CalcStageFlow(cur, next, flow_x, flow_y);
+		CalcStageFlow(next, cur, flow_inv_x, flow_inv_y);
+
+		CalcOcclusion(flow_x, flow_y, flow_inv_x, flow_inv_y, isOccluded);
+		CalcOcclusion(flow_inv_x, flow_inv_y, flow_x, flow_y, isOccludedInv);
+
+		BilateralFilter(flow_x, flow_y, *frames[0], *frames[1], confidence, isOccluded);
+		BilateralFilter(flow_inv_x, flow_inv_y, *frames[1], *frames[0], confidence_inv, isOccludedInv);
+
 	}
+
+	vel_x = flow_x;
+	vel_y = flow_y;
 
 }
--- AvanceEthan/Trabajo_Terminal_I/simple_flow.h
+++ AvanceEthan/Trabajo_Terminal_I/simple_flow.h
@@ -18,11 +18,7 @@ public:
 	void RemoveFrame();
 	// Flow calculation method
 	void CalculateFlow(cv::Mat& vel_x, cv::Mat& vel_y);
-	double getSmoothness(Frame &f1, Frame &f2, int x0, int y0, int x, int y);
-	cv::Mat UpscaleFlow(cv::Mat& flow, int new_cols, int new_rows, Frame &image, cv::Mat& confidence);
-	void CalcStageFlow(Frame& cur, Frame& next, cv::Mat& flow_x, cv::Mat& flow_y);
-	void CrossBilateralFilter(cv::Mat &orig, Frame &edge, cv::Mat& confidence, cv::Mat& dest);
-	void CalcMultiStageFlow(cv::Mat& vel_x, cv::Mat& vel_y);
+	
 private:
 	std::vector<Frame*> frames;
 	void BuildPyramid(Frame &src, std::vector<Frame>& pyramid);
@@ -30,7 +26,14 @@ private:
 	double GetWr(std::vector<int> &energyArray);
 	double GetWd(int x0, int y0, int x, int y);
 	double GetWc(Frame &f1, int x0, int y0, int x, int y);
-	double SimpleFlow::GetWc(cv::Mat &f1, int x0, int y0, int x, int y);
+	double GetWc(cv::Mat &f1, int x0, int y0, int x, int y);
+	double getSmoothness(Frame &f1, Frame &f2, int x0, int y0, int x, int y);
+	cv::Mat UpscaleFlow(cv::Mat& flow, int new_cols, int new_rows, Frame &image, cv::Mat& confidence);
+	void CalcStageFlow(Frame& cur, Frame& next, cv::Mat& flow_x, cv::Mat& flow_y);
+	void BilateralFilter(cv::Mat flow_x, cv::Mat flow_y, Frame cur, Frame next, cv::Mat confidence, std::vector< std::vector<bool> >& isOccludedPixel);
+	void CrossBilateralFilter(cv::Mat &orig, Frame &edge, cv::Mat& confidence, cv::Mat& dest);
+	void CalcOcclusion(cv::Mat& vel_x, cv::Mat& vel_y, cv::Mat& vel_x_inv, cv::Mat& vel_y_inv, std::vector< std::vector<bool> >& isOccludedPixel);
+	void CalcConfidence(Frame& cur, Frame& next, cv::Mat& flow_x, cv::Mat& flow_y, cv::Mat& confidence);
 	static const int NeighborhoodSize = 5;
 	static const int Layers = 5;
 	static const double rd;
