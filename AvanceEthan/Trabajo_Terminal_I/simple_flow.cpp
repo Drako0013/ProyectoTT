51f9417a52964b0f58faece21f5e52ced5321fb4
Adonais Romero González <adonais.neoadonis@gmail.com>
2015-01-14 00:28:49 -0600

    First implementation of multi-stage SF

--- AvanceEthan/Trabajo_Terminal_I/simple_flow.cpp
+++ AvanceEthan/Trabajo_Terminal_I/simple_flow.cpp
@@ -59,7 +59,102 @@ double SimpleFlow::getSmoothness(Frame &f1, Frame &f2, int x0, int y0, int u, in
 	return result;
 }
 
+void SimpleFlow::BuildPyramid(Frame &src, std::vector<Frame>& pyramid) {
+	pyramid.push_back(src);
+	for (int i = 1; i <= SimpleFlow::Layers; ++i) {
+		Frame prev = pyramid[i - 1];
+		if (prev.Rows() <= 1 || prev.Columns() <= 1) {
+			break;
+		}
+		Frame next;
+		next.Rescale((prev.Rows() + 1) / 2, (prev.Columns() + 1) / 2);
+		pyramid.push_back(next);
+	}
+}
+
+void SimpleFlow::CrossBilateralFilter(cv::Mat &orig, Frame &edge, cv::Mat& confidence, cv::Mat& dest) {
+	const int n = SimpleFlow::NeighborhoodSize;
+	int rows = edge.Rows();
+	int cols = edge.Columns();
+	for (int x = 0; x < rows; ++x) {
+		double* ptr_dest = dest.ptr<double>(x);
+		double* ptr_orig = orig.ptr<double>(x);
+		double* ptr_wr = orig.ptr<double>(x);
+		for (int y = 0; y < cols; ++y, ++ptr_dest, ++ptr_orig, ++ptr_wr) {
+			double result = 0.0f;
+			for (int i = -n; i <= n; ++i) {
+				for (int j = -n; j <= n; ++j) {
+					if (x + i >= 0 && x + i < rows && y + j >= 0 && y + j < cols) {
+						result += (*ptr_orig) * GetWd(x, y, x + i, y + j) * GetWc(edge, x, y, x + i, y + j) * (*ptr_wr);
+					}
+				}
+			}
+			*ptr_dest = result;
+		}
+	}
+}
+
+
+cv::Mat SimpleFlow::UpscaleFlow(cv::Mat& flow, int new_cols, int new_rows, Frame &image, cv::Mat& confidence) {
+	cv::Mat orig_flow = flow;
+	CrossBilateralFilter(orig_flow, image, confidence, flow);
+	cv::Mat new_flow;
+	resize(flow, new_flow, cv::Size(new_cols, new_rows), 0, 0, cv::INTER_NEAREST);
+	new_flow *= 2;
+	return new_flow;
+}
+
+void SimpleFlow::CalcStageFlow(Frame& cur, Frame& next, cv::Mat& flow_x, cv::Mat& flow_y) {
+	// TODO: Refactor code
+}
+
+void SimpleFlow::CalcMultiStageFlow(cv::Mat& vel_x, cv::Mat& vel_y) {
+	cv::Mat flow_x, flow_y, flow_inv_x, flow_inv_y, confidence, confidence_inv;
+
+	//For now...
+	confidence = cv::Mat::ones(frames[0]->GetMatrix().size(), CV_64F);
+	confidence_inv = cv::Mat::ones(frames[0]->GetMatrix().size(), CV_64F);
+
+	std::vector<Frame> pyramid_cur, pyramid_next;
+
+	BuildPyramid(*frames[0], pyramid_cur);
+	BuildPyramid(*frames[1], pyramid_next);
+
+	// Refactor SimpleFlow for one stage. We'll be using it a lot...
+	CalcStageFlow(pyramid_cur.back(), pyramid_next.back(), flow_x, flow_y);
+	CalcStageFlow(pyramid_next.back(), pyramid_cur.back(), flow_inv_x, flow_inv_y);
+
+	for (int l = pyramid_cur.size() - 2; l >= 0; --l) {
+		Frame cur = pyramid_cur[l];
+		Frame next = pyramid_next[l];
+		Frame p_cur = pyramid_cur[l + 1];
+		Frame p_next = pyramid_next[l + 1];
+
+		const int curr_rows = cur.Rows();
+		const int curr_cols = cur.Columns();
+
+		flow_x = UpscaleFlow(flow_x, curr_rows, curr_cols, p_cur, confidence);
+		flow_inv_x = UpscaleFlow(flow_x, curr_rows, curr_cols, p_next, confidence_inv);
+		flow_y = UpscaleFlow(flow_y, curr_rows, curr_cols, p_cur, confidence);
+		flow_inv_y = UpscaleFlow(flow_y, curr_rows, curr_cols, p_next, confidence_inv);
+
+
+		//CalcConfidence(cur, curr_to, flow, confidence, max_flow);
+		CalcStageFlow(cur, next, flow_x, flow_y);
+
+		//calcConfidence(curr_to, cur, flow_inv, confidence_inv, max_flow);
+		CalcStageFlow(next, cur, flow_inv_x, flow_inv_y);
+
+	}
+
+	vel_x = flow_x;
+	vel_y = flow_y;
+}
+
+
 void SimpleFlow::CalculateFlow(cv::Mat& vel_x, cv::Mat& vel_y) {
+
+	// Following code needs to be refactored:
 
 	const int n = SimpleFlow::NeighborhoodSize;
 
@@ -140,7 +235,8 @@ void SimpleFlow::CalculateFlow(cv::Mat& vel_x, cv::Mat& vel_y) {
 				the result by applying a bilateral filter on the flow vectors.
 				For this operation, we discard the occluded pixels, and use
 				the weights wd and wc with an additional weight wr that represents
-				how reliable is our flow estimate at (x, y):
+				how reliable is our flow estimate at (x, y):
+
 			*/
 
 			if( occlusion < occlusion_limit ){
--- AvanceEthan/Trabajo_Terminal_I/simple_flow.h
+++ AvanceEthan/Trabajo_Terminal_I/simple_flow.h
@@ -3,7 +3,7 @@
 
 #include <algorithm>
 #include <cmath>
-#include <map>
+#include <vector>
 #include <utility>
 #include <limits>
 
@@ -19,13 +19,19 @@ public:
 	// Flow calculation method
 	void CalculateFlow(cv::Mat& vel_x, cv::Mat& vel_y);
 	double getSmoothness(Frame &f1, Frame &f2, int x0, int y0, int x, int y);
+	cv::Mat UpscaleFlow(cv::Mat& flow, int new_cols, int new_rows, Frame &image, cv::Mat& confidence);
+	void CalcStageFlow(Frame& cur, Frame& next, cv::Mat& flow_x, cv::Mat& flow_y);
+	void CrossBilateralFilter(cv::Mat &orig, Frame &edge, cv::Mat& confidence, cv::Mat& dest);
+	void CalcMultiStageFlow(cv::Mat& vel_x, cv::Mat& vel_y);
 private:
 	std::vector<Frame*> frames;
+	void BuildPyramid(Frame &src, std::vector<Frame>& pyramid);
 	int GetEnergy(Frame &f1, int x1, int y1, Frame &f2, int x2, int y2);
 	double GetWr(std::vector<int> &energyArray);
 	double GetWd(int x0, int y0, int x, int y);
 	double GetWc(Frame &f1, int x0, int y0, int x, int y);
 	static const int NeighborhoodSize = 5;
+	static const int Layers = 5;
 	const double rd = 5.5;
 	const double rc = 0.08;
 };
